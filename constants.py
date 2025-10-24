6e22a00e41a842c927947ad9364dad2247518903
jona <jona@neocybernetica.com>
2025-10-24 12:40:54 +0200

    7 rooms

--- constants.py
+++ constants.py
@@ -4,4 +4,4 @@ TILE_SIZE = 16
 MAP_WIDTH = 10
 MAP_HEIGHT = 10
 DOOR = "door"
-MAX_FLOORS = max(1, int(os.getenv("DUNGEON_BREACH_ROOMS", "3")))
+MAX_FLOORS = max(1, int(os.getenv("DUNGEON_BREACH_ROOMS", "7")))
